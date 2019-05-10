6131fe4c46cf042b04e74004dd82e74c95e8c8f7
Antoine Lamielle <gh@0x010c.fr>
2019-05-10 18:58:59 +1200

    Add 3 tabs to the toolbar's popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,47 @@
 (async function() {
-	var backgroundPage = await browser.runtime.getBackgroundPage();
+	var ui,
+		backgroundPage = await browser.runtime.getBackgroundPage();
 
-	async function initOptions() {
+	var UI = function () {
+		// Setup the main tab layout
+		this.viewTab = new OO.ui.TabPanelLayout( 'view', { label: 'Consulter' } );
+		this.historyTab = new OO.ui.TabPanelLayout( 'history', { label: 'Historique' } );
+		this.paramTab = new OO.ui.TabPanelLayout( 'param', { label: 'Paramètres', classes: [ 'popup-tab-param' ] } );
+
+		this.indexLayout = new OO.ui.IndexLayout( { autoFocus: false, classes: [ 'popup-tabs' ] } );
+		this.indexLayout.addTabPanels( [ this.viewTab, this.historyTab, this.paramTab ] );
+		$( '#popup-loaded' ).append( this.indexLayout.$element );
+
+		// Setup the view tab
+		this.initView();
+
+		// Setup the history tab
+		this.initHistory();
+
+		// Setup the param tab
+		this.initParam();
+
+		// show the UI we have just build
+		this.switchPanel( 'loaded' );
+	};
+
+	UI.prototype.initView = async function () {
+		tabs = await browser.tabs.query({active: true, currentWindow: true});
+	  	selection = await browser.tabs.sendMessage( tabs[ 0 ].id, {
+			command: "signit.getSelection",
+		} );
+		word = backgroundPage.normalize( selection );
+		files = backgroundPage.wordToFiles ( word );
+		content = new SignItCoreContent();
+		content.refresh( word, files );
+		this.viewTab.$element.append( content.getContainer() );
+	};
+
+	UI.prototype.initHistory = function () {
+
+	};
+
+	UI.prototype.initParam = function () {
 		/* Language picker */
 		var i, languageDropdown, languageLayout,
 		 	items = [];
@@ -21,28 +61,16 @@
 			align: 'top',
 			help: 'Change la langue des signes utilisée dans les vidéos.',
 			//helpInline: true
-		} ),
-
-		$( '#popup-options-languages' ).append( languageLayout.$element );
-
-		switchTab( 'options' );
-
-		tabs = await browser.tabs.query({active: true, currentWindow: true});
-	  	selection = await browser.tabs.sendMessage( tabs[ 0 ].id, {
-			command: "signit.getSelection",
 		} );
-		word = backgroundPage.normalize( selection );
-		files = backgroundPage.wordToFiles ( word );
-		content = new SignItCoreContent();
-		content.refresh( word, files );
-		$( '#popup-options' ).append( content.getContainer() );
-	}
 
-	function switchTab( tab ) {
+		this.paramTab.$element.append( languageLayout.$element );
+	};
+
+	UI.prototype.switchPanel = function( tab ) {
 		$( '#popup-loading' ).hide();
-		$( '#popup-options' ).hide();
+		$( '#popup-loaded' ).hide();
 		$( '#popup-' + tab ).show();
-	}
+	};
 
 	async function changeLanguage( item ) {
 		var newLang = item.getData();
@@ -50,17 +78,17 @@
 			return;
 		}
 
-		switchTab( 'loading' );
+		ui.switchPanel( 'loading' );
 		await backgroundPage.changeLanguage( newLang );
-		switchTab( 'options' );
+		ui.switchPanel( 'loaded' );
 	}
 
 	function waitWhileLoading() {
 		console.log( 'loading' );
-		if ( backgroundPage.state !== 'ready' ) {
-			setTimeout( waitWhileLoading, 100 );
+		if ( backgroundPage.state === 'ready' ) {
+			ui = new UI();
 		} else {
-			initOptions();
+			setTimeout( waitWhileLoading, 100 );
 		}
 	}
 	waitWhileLoading()
