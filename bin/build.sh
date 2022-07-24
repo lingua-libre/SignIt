c7dfde814d678652fbc65d8808fcf6421307e76b
hugolpz <hugo.lpz@gmail.com>
2022-07-24 12:07:50 +0200

    edit build to solve  oojs v.6.0.0 removal of oojs.jquery

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@
 mkdir -p lib/ ;
 cp -v node_modules/webextension-polyfill/dist/browser-polyfill.min.js lib/ ;
 cp -v node_modules/jquery/dist/jquery.min.js lib/ ;
-cp -v node_modules/oojs/dist/oojs.jquery.min.js lib/ ;
+cp -v bin/oojs.jquery.min.js lib/ ;    # a deprecated flavor of jquery, see https://github.com/lingua-libre/SignIt/issues/9
 cp -v node_modules/oojs-ui/dist/oojs-ui.min.js lib/ ;
 cp -v node_modules/oojs-ui/dist/oojs-ui-wikimediaui.min.js lib/ ;
 cp -v node_modules/oojs-ui/dist/oojs-ui-wikimediaui.min.css lib/ ;
