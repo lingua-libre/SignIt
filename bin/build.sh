863f581b0054f77b2c385f32e0a27063d0207dc6
hugolpz <hugo.lpz@gmail.com>
2022-08-02 16:10:12 +0200

    add banana-i18n.js library for internationalization

--- bin/build.sh
+++ bin/build.sh
@@ -7,5 +7,6 @@ cp -v bin/oojs.jquery.min.js lib/ ;    # a deprecated flavor of jquery, see http
 cp -v node_modules/oojs-ui/dist/oojs-ui.min.js lib/ ;
 cp -v node_modules/oojs-ui/dist/oojs-ui-wikimediaui.min.js lib/ ;
 cp -v node_modules/oojs-ui/dist/oojs-ui-wikimediaui.min.css lib/ ;
+cp -v node_modules/banana-i18n/banana-i18n.js lib/ ;
 # Icons, this is a ugly hack, so when oojs from the add-on mess up Wikimedia oojs, there are still icons on the new path: the cat fall back on its legs !
 cp -v node_modules/oojs-ui/dist/themes/wikimediaui/images/icons/* lib/themes/wikimediaui/images/icons/ ;
