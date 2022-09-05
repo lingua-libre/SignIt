07a8eecd5b15aaf02852710a7e04fc846a1573d4
hugolpz <hugo.lpz@gmail.com>
2022-09-05 23:41:35 +0200

    split wikicode into wiki + i18n locale ; filter signLanguages;

--- background-script.js
+++ background-script.js
@@ -79,39 +79,40 @@ var state = 'up', // up, loading, ready, error
 /* i18n context ************************************************** */
 	// Get UI languages with translations on github
 	var supportedUiLanguages = [
-		{ wdQid:"Q13955",wikimediaCode:"ar",labelFR:"Arabe",labelEN:"Arabic",nativeName:"اللُّغَة العَرَبِيّة", wikt:"" },
-		{ wdQid:"Q9610",wikimediaCode:"bn",labelFR:"Bengali",labelEN:"Bengali",nativeName:"বাংলা", wikt:"#Francais" },
-		{ wdQid:"Q188",wikimediaCode:"de",labelFR:"Allemand",labelEN:"German",nativeName:"Deutsch", wikt:"" },
-		{ wdQid:"Q1860",wikimediaCode:"en",labelFR:"Anglais",labelEN:"English",nativeName:"English", wikt:"#English" },
-		//{ wdQid:"Q9240",wikimediaCode:"id",labelFR:"indonésien",labelEN:"Indonesian",nativeName:"Bahasa Indonesia", wikt:"" },
-		{ wdQid:"Q652",wikimediaCode:"it",labelFR:"Italien",labelEN:"Italian",nativeName:"Italiano", wikt:"" },
-		{ wdQid:"Q1321",wikimediaCode:"es",labelFR:"Espagnol",labelEN:"Spanish",nativeName:"Español", wikt: "#Español" },
-		{ wdQid:"Q150",wikimediaCode:"fr",labelFR:"Français",labelEN:"French",nativeName:"Français", wikt:"" },
-		{ wdQid:"Q9288",wikimediaCode:"he",labelFR:"Hébreu",labelEN:"Hebrew",nativeName:"עברית", wikt:"" },
-		{ wdQid:"Q1568",wikimediaCode:"hi",labelFR:"Hindi",labelEN:"Hindi",nativeName:"मानक हिन्दी", wikt:"" },
-		//{ wdQid:"Q9067",wikimediaCode:"hu",labelFR:"hongrois",labelEN:"Hungarian",nativeName:"Magyar", wikt:"" },
-		{ wdQid:"Q5287",wikimediaCode:"ja",labelFR:"Japonais",labelEN:"Japanese",nativeName:"日本語", wikt:"" },
-		{ wdQid:"Q9252",wikimediaCode:"kk",labelFR:"Kazakh",labelEN:"Kazakh",nativeName:"Казақша", wikt:"" },
-		{ wdQid:"Q9176",wikimediaCode:"ko",labelFR:"Coréen",labelEN:"Korean",nativeName:"한국어", wikt:"" },
-		//{ wdQid: "Q7930",wikimediaCode:"mg",labelFR:"malgache",labelEN:"Malagasy",nativeName:"Fiteny Magalasy",wikt:"" },
-		{ wdQid:"Q9296",wikimediaCode:"mk",labelFR:"Macédonien",labelEN:"Macedonian",nativeName:"Македонски", wikt:"" },
-		{ wdQid:"Q25167",wikimediaCode:"nb",labelFR:"Bokmål",labelEN:"Bokmål",nativeName:"Bokmål", wikt:"" },
-		{ wdQid:"Q5146",wikimediaCode:"pt",labelFR:"Portugais",labelEN:"Portuguese",nativeName:"Português", wikt:"" },
-		{ wdQid:"Q7737",wikimediaCode:"ru",labelFR:"Russe",labelEN:"Russian",nativeName:"Русский язык", wikt:"" },
-		{ wdQid:"Q33973",wikimediaCode:"scn",labelFR:"Sicilien",labelEN:"Sicilian",nativeName:"Sicilianu", wikt:"" },
-		{ wdQid:"Q9027",wikimediaCode:"sv",labelFR:"Suédois",labelEN:"Swedish",nativeName:"Svenska", wikt:"" },
-		//{ wdQid:"Q7838",wikimediaCode:"sw",labelFR:"swahili",labelEN:"Swahili",nativeName:"Kiswahili", wikt:"" },
-		//{ wdQid:"Q9267",wikimediaCode:"tk",labelFR:"turkmène",labelEN:"Turkmen",nativeName:"Türkmençe",wikt:"" },
-		{ wdQid:"Q34057",wikimediaCode:"tl",labelFR:"Tagalog",labelEN:"Tagalog",nativeName:"Wikang Tagalog", wikt:"" },
-		{ wdQid:"Q256",wikimediaCode:"tr",labelFR:"Turc",labelEN:"Turkish",nativeName:"Türkçe", wikt:"" },		
-		{ wdQid:"Q18130932",wikimediaCode:"zh-hant",labelFR:"Chinois traditionel",labelEN:"Traditional Chinese",nativeName:"中文 (繁體)", wikt:"" },
-		{ wdQid:"Q13414913",wikimediaCode:"zh-hans",labelFR:"Chinois moderne",labelEN:"Modern Chinese",nativeName:"中文 (简体)", wikt:"" },
+		{ wdQid:"Q13955",wiki:"ar",i18n:"ar",labelFR:"Arabe",labelEN:"Arabic",nativeName:"اللُّغَة العَرَبِيّة", wikt:"" },
+		{ wdQid:"Q9610",wiki:"bn",i18n:"bn",labelFR:"Bengali",labelEN:"Bengali",nativeName:"বাংলা", wikt:"" },
+		{ wdQid:"Q188",wiki:"de",i18n:"de",labelFR:"Allemand",labelEN:"German",nativeName:"Deutsch", wikt:"" },
+		{ wdQid:"Q1860",wiki:"en",i18n:"en",labelFR:"Anglais",labelEN:"English",nativeName:"English", wikt:"" },
+		{ wdQid:"Q1321",wiki:"es",i18n:"es",labelFR:"Espagnol",labelEN:"Spanish",nativeName:"Español", wikt: "" },
+		{ wdQid:"Q150",wiki:"fr",i18n:"fr",labelFR:"Français",labelEN:"French",nativeName:"Français", wikt:"" },
+		{ wdQid:"Q9288",wiki:"he",i18n:"he",labelFR:"Hébreu",labelEN:"Hebrew",nativeName:"עברית", wikt:"" },
+		{ wdQid:"Q1568",wiki:"hi",i18n:"hi",labelFR:"Hindi",labelEN:"Hindi",nativeName:"मानक हिन्दी", wikt:"" },
+		//{ wdQid:"Q9067",wiki:"hu",i18n:"hu",labelFR:"hongrois",labelEN:"Hungarian",nativeName:"Magyar", wikt:"" },
+		//{ wdQid:"Q9240",wiki:"id",i18n:"id",labelFR:"indonésien",labelEN:"Indonesian",nativeName:"Bahasa Indonesia", wikt:"" },
+		{ wdQid:"Q652",wiki:"it",i18n:"it",labelFR:"Italien",labelEN:"Italian",nativeName:"Italiano", wikt:"" },
+		{ wdQid:"Q5287",wiki:"ja",i18n:"ja",labelFR:"Japonais",labelEN:"Japanese",nativeName:"日本語", wikt:"" },
+		{ wdQid:"Q9252",wiki:"kk",i18n:"kk-cyrl",labelFR:"Kazakh",labelEN:"Kazakh",nativeName:"Казақша", wikt:"" },
+		{ wdQid:"Q9176",wiki:"ko",i18n:"ko",labelFR:"Coréen",labelEN:"Korean",nativeName:"한국어", wikt:"" },
+		//{ wdQid: "Q7930",wiki:"mg",i18n:"mg",labelFR:"malgache",labelEN:"Malagasy",nativeName:"Fiteny Magalasy",wikt:"" },
+		{ wdQid:"Q9296",wiki:"mk",i18n:"mk",labelFR:"Macédonien",labelEN:"Macedonian",nativeName:"Македонски", wikt:"" },
+		{ wdQid:"Q25167",wiki:"nb",i18n:"nb",labelFR:"Bokmål",labelEN:"Bokmål",nativeName:"Bokmål", wikt:"" },
+		{ wdQid:"Q5146",wiki:"pt",i18n:"pt",labelFR:"Portugais",labelEN:"Portuguese",nativeName:"Português (pt)", wikt:"" },
+		{ wdQid:"Q5146",wiki:"pt",i18n:"pt-br",labelFR:"Portugais",labelEN:"Portuguese",nativeName:"Português (br)", wikt:"" },
+		{ wdQid:"Q7737",wiki:"ru",i18n:"ru",labelFR:"Russe",labelEN:"Russian",nativeName:"Русский язык", wikt:"" },
+		{ wdQid:"Q33973",wiki:"scn",i18n:"scn",labelFR:"Sicilien",labelEN:"Sicilian",nativeName:"Sicilianu", wikt:"" },
+		{ wdQid:"Q9027",wiki:"sv",i18n:"sv",labelFR:"Suédois",labelEN:"Swedish",nativeName:"Svenska", wikt:"" },
+		//{ wdQid:"Q7838",wiki:"sw",i18n:"sw",labelFR:"swahili",labelEN:"Swahili",nativeName:"Kiswahili", wikt:"" },
+		//{ wdQid:"Q9267",wiki:"tk",i18n:"tk",labelFR:"turkmène",labelEN:"Turkmen",nativeName:"Türkmençe",wikt:"" },
+		{ wdQid:"Q34057",wiki:"tl",i18n:"tl",labelFR:"Tagalog",labelEN:"Tagalog",nativeName:"Wikang Tagalog", wikt:"" },
+		{ wdQid:"Q256",wiki:"tr",i18n:"tr",labelFR:"Turc",labelEN:"Turkish",nativeName:"Türkçe", wikt:"" },		
+		{ wdQid:"Q18130932",wiki:"zh",i18n:"zh-hant",labelFR:"Chinois traditionel",labelEN:"Traditional Chinese",nativeName:"中文 (繁體)", wikt:"" },
+		{ wdQid:"Q13414913",wiki:"zh",i18n:"zh-hans",labelFR:"Chinois moderne",labelEN:"Modern Chinese",nativeName:"中文 (简体)", wikt:"" },
 
-		//{ wdQid:"",wikimediaCode:"",labelFR:"",labelEN:"",nativeName:"", wikt:"" },
+		//{ wdQid:"",wiki:"",i18n:"",labelFR:"",labelEN:"",nativeName:"", wikt:"" },
 	];
 
 	var filterArrayBy = function (arr, key, value){
-		return arr.filter(item => (item[key]==value) )[0]
+		return arr.filter(item => (item[key]==value) )
 	};
 
 	// Init internationalisation support with Banana-i18n.js
@@ -175,7 +176,6 @@ async function getSignLanguagesWithVideos() {
 			sparqlEndpoints.lingualibre.url, 
 			{ format: 'json', query: sparqlSignLanguagesQuery } 
 		);
-
 	// create signLanguages objects
 	for ( i = 0; i < response.results.bindings.length; i++ ) {
 		var signLanguageRaw = response.results.bindings[ i ];
@@ -183,6 +183,10 @@ async function getSignLanguagesWithVideos() {
 		signLanguage = { wdQid: signLanguageRaw.id.value.split( '/' ).pop(), nativeName: signLanguageRaw.idLabel.value }
 		signLanguages[i] = signLanguage;
 	}
+	// TEMPORARY, WHEN ONLY LSF HAS VIDEOS
+	signLanguages = filterArrayBy(signLanguages,"wdQid", "Q99628");
+	console.log(signLanguages)
+
 	return signLanguages;
 }
 
@@ -248,7 +252,7 @@ async function fetchJS(filepath) {
 		return content;
 	} catch (error) { console.error(error); }
 }
- messages = await fetchJS(`i18n/${iso}.json`); */
+ messages = await fetchJS(`i18n/${locale}.json`); */
 
 // Loading all UI translations
 async function loadI18nLocalization( uiLanguageQid ) {
@@ -259,20 +263,20 @@ async function loadI18nLocalization( uiLanguageQid ) {
 
 	state = 'loading';
 	
-	// Get iso code and corresponding wiktionary
+	// Get locale code and corresponding wiktionary
 	var lang = supportedUiLanguages.filter(item => (item.wdQid==uiLanguageQid) );
-	iso = lang[0].wikimediaCode;
-	console.log("iso",iso)
+	locale = lang[0].i18n;
+	console.log("locale",locale)
 
 	// Load i18n messages
-	const res = await fetch(`i18n/${iso}.json`)
+	const res = await fetch(`i18n/${locale}.json`)
 	messages = await res.json();
 	console.log("messages",messages["si-popup-settings-title"])
 	// Load messages into localisation
-	banana.load(messages, iso); // Load localized messages (chould be conditional to empty)
+	banana.load(messages, locale); // Load localized messages (chould be conditional to empty)
 
 	// Declare localisation
-	banana.setLocale(iso); // Change to new locale
+	banana.setLocale(locale); // Change to new locale
 	
 	state = 'ready';
 
@@ -340,8 +344,8 @@ browser.runtime.onMessage.addListener( async function ( message ) {
 	 // When message 'signit.i18n' is heard, returns banada object
 	else if ( message.command === 'signit.getfilesb' ) {
 		console.log('bg>signit.getfilesB')
-		// var iso = await getStoredParam( 'uiLanguage' )
-		// loadI18nLocalization(iso);
+		// var locale = await getStoredParam( 'uiLanguage' )
+		// loadI18nLocalization(locale);
 		return banana;
 	}
 	
