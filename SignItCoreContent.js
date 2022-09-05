4e4ef05626f001eea5897c07c5addc3d2c112751
hugolpz <hugo.lpz@gmail.com>
2022-09-05 19:29:03 +0200

    harmonize variables

--- SignItCoreContent.js
+++ SignItCoreContent.js
@@ -39,15 +39,16 @@ var SignItCoreContent = function () {
 	this.contributeButton = new OO.ui.ButtonWidget( optionsContribute );
 
 	this.$title = this.$container.children( 'h1' );
-	this.$wtdef = this.$container.find( '.signit-definition-text' );
-	this.$wtsource = this.$container.find( '.signit-definition-source a' );
-	this.$leftPanelNoVideo = this.$container.find( '.signit-novideo' ).append( this.contributeButton.$element );
-	this.$leftPanelContent = this.$container.find( '.signit-video' );
-	this.$rightPanelContent = this.$container.find( '.signit-definition' );
-	this.$rightPanelSpinner = this.$container.find( '.signit-loading' );
-	this.$rightPanelError = this.$container.find( '.signit-error' );
 
-	this.videosGallery = new SignItVideosGallery( this.$leftPanelContent );
+	this.$videosPanelNoVideo = this.$container.find( '.signit-novideo' ).append( this.contributeButton.$element );
+	this.$videosPanelContent = this.$container.find( '.signit-video' );
+	this.$videosPanelGallery = new SignItVideosGallery( this.$videosPanelContent );
+
+	this.$definitionPanelContent = this.$container.find( '.signit-definition' );
+	this.$definitionPanelText = this.$container.find( '.signit-definition-text' );
+	this.$definitionPanelSource = this.$container.find( '.signit-definition-source a' );
+	this.$definitionPanelSpinner = this.$container.find( '.signit-loading' );
+	this.$definitionPanelError = this.$container.find( '.signit-error' );
 
 	// this.contributeButton.on( 'click', function () {
 	//	// TODO: Do something
@@ -61,21 +62,21 @@ SignItCoreContent.prototype.refresh = function ( title, files ) {
 	this.setWiktionaryContent( title );
 	// Media panel
 	if ( files.length > 0 ) {
-		this.videosGallery.refresh( files );
-		this.$leftPanelNoVideo.hide();
-		this.$leftPanelContent.show();
+		this.$videosPanelGallery.refresh( files );
+		this.$videosPanelNoVideo.hide();
+		this.$videosPanelContent.show();
 	} else {
-		this.$leftPanelContent.hide();
-		this.$leftPanelNoVideo.show();
+		this.$videosPanelContent.hide();
+		this.$videosPanelNoVideo.show();
 	}
 };
 
 SignItCoreContent.prototype.setWiktionaryContent = async function ( title ) {
-	var content, $wiktSection, definition, result;//, wtsource;
+	var content, $wiktSection, definition, result;//, definitionSource;
 
-	this.$rightPanelContent.hide();
-	this.$rightPanelError.hide();
-	this.$rightPanelSpinner.show();
+	this.$definitionPanelContent.hide();
+	this.$definitionPanelError.hide();
+	this.$definitionPanelSpinner.show();
 
 	try {
 		result = await $.post( 'https://fr.wiktionary.org/w/api.php', {
@@ -99,8 +100,8 @@ SignItCoreContent.prototype.setWiktionaryContent = async function ( title ) {
 			return this.setWiktionaryContent( title.toLowerCase() );
 		}
 
-		this.$rightPanelSpinner.hide();
-		this.$rightPanelError.show();
+		this.$definitionPanelSpinner.hide();
+		this.$definitionPanelError.show();
 		return;
 	}
 
@@ -110,11 +111,11 @@ SignItCoreContent.prototype.setWiktionaryContent = async function ( title ) {
 	$wiktSection = content.find( "#Français" /* banana.i18n("si-overlay-coreContent-right-wikt-section-id") */ ).parent().next();
 	definition = $wiktSection.find( '.titredef' ).parent().parent().nextUntil( 'h2, h3, h4' ).filter( 'p, ol' );
 
-	this.$wtsource.attr( 'href', `https://fr.wiktionary.org/wiki/${ title }` );
+	this.$definitionPanelSource.attr( 'href', `https://fr.wiktionary.org/wiki/${ title }` );
 
-	this.$wtdef.html( definition );
-	this.$rightPanelSpinner.hide();
-	this.$rightPanelContent.show();
+	this.$definitionPanelText.html( definition );
+	this.$definitionPanelSpinner.hide();
+	this.$definitionPanelContent.show();
 };
 
 SignItCoreContent.prototype.getContainer = function () {
