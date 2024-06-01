75e7695793043d4aee2f0c37c51884d2e352830b
Hugo Lopez <hugo.lpz@gmail.com>
2024-06-01 22:42:13 +0200

    ./_locales/buildLocales.sh convert keys to sname_case

--- _locales/buildLocales.sh
+++ _locales/buildLocales.sh
@@ -3,7 +3,7 @@ mkdir -p _locales                  # Create directory if not there
 for file in ./i18n/*.json; do      # Loop
   iso=$(basename "$file" .json)    # Extract basename
   mkdir -p "_locales/$iso"         # Create corresponding dir
-  jq 'del(.["@metadata"]) | with_entries(.value |= {"message": .}) | with_entries(.value.description |= "")' "$file" > "_locales/$iso/messages.json"
+  jq 'del(.["@metadata"]) | with_entries(.key |= gsub("-"; "_") | .value |= {"message": .}) | with_entries(.value.description |= "")' "$file" > "_locales/$iso/messages.json"
 done
 echo "Done creating ./_locales/{iso}/messages.json"
 
