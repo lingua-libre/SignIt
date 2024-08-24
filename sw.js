e044638dce6635aac9f0aaf4596ff87e22ddc9b3
kabir-afk <kabirsingh12b@gmail.com>
2024-08-24 13:56:03 +0530

    chore : filtering ArSL and fsl

--- sw.js
+++ sw.js
@@ -484,7 +484,7 @@ async function setState(value) {
 
       const data = await response.json(); // Await the parsed JSON response
 
-      let signLanguages = [];
+      let signLanguages = [] ,frenchSignLanguage = [],armenianSignLanguage = [];
       for (let i = 0; i < data.results.bindings.length; i++) {
         const signLanguageRaw = data.results.bindings[i];
         const signLanguage = {
@@ -495,10 +495,9 @@ async function setState(value) {
       }
 
       // Temporary filtering (assuming filterArrayBy is available)
-      signLanguages = filterArrayBy(signLanguages, "wdQid", "Q99628");
-
-      console.log(signLanguages);
-      return signLanguages;
+      frenchSignLanguage = filterArrayBy(signLanguages, "wdQid", "Q99628");
+      armenianSignLanguage = filterArrayBy(signLanguages, "wdQid", "Q1379196");
+      return [...frenchSignLanguage,...armenianSignLanguage];
     } catch (error) {
       console.error("Error fetching or processing data:", error);
     }
